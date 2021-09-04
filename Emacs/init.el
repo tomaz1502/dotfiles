b0e071fd50a1be84007ce7fa980cb0ab1a5e6719
Tomaz Gomes <tomgm1502@gmail.com>
2021-09-04 03:33:15 +0000

    update init.el

--- Emacs/init.el
+++ Emacs/init.el
@@ -1,30 +1,25 @@
 ;; Remove welcome message
-(setq inhibit-startup-message t)
+;; (setq inhibit-startup-message t)
 
-;; Remove menus
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-
-;; Remove scrolling bar
 (scroll-bar-mode -1)
 
-(set-face-attribute 'default nil :height 120)
+(set-face-attribute 'default nil :height 160)
+(set-frame-font "Fira Mono" nil t)
 
-(set-default-font "Fira Mono 14")
-;; (set-frame-font "Source Code Pro" nil t)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
 
-;; theme
-;;(load-theme 'base16-default-dark)
-
-;; line numbers
 (global-display-line-numbers-mode)
-(setq display-line-numbers-type 'relative)
+;; (setq display-line-numbers-type 'relative)
 
 (require 'package)
 (setq package-enable-at-startup nil)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
 
 (package-initialize)
 
@@ -38,17 +33,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default)))
+   '("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default))
  '(package-selected-packages
-   (quote
-    (base16-theme gruvbox-theme emacs-gruvbox-themes magit key-chord all-the-icons neotree auto-complete evil use-package))))
+   '(eshell-syntax-highlighting helm base16-theme magit key-chord auto-complete evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-hl-line-mode t)
 
 (use-package evil
   :ensure t
@@ -63,38 +58,70 @@
 (use-package auto-complete
   :ensure t
   :init
-  (progn
-    (ac-config-default)
-    (global-auto-complete-mode t)
-  )
+    (progn
+	(ac-config-default)
+	(global-auto-complete-mode t)
+    )
 )
 
-(use-package neotree
-  :config (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-  :ensure t)
-
-(use-package all-the-icons
-  :ensure t)
-
 (use-package key-chord
+  :config (setq key-chord-two-keys-delay 0.5)
+  :init
+    (progn
+      (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+      (key-chord-mode 1)
+    )
   :ensure t)
 
 (use-package magit
   :ensure t)
 
-(use-package gruvbox-theme
-  :ensure t)
-
 (use-package base16-theme
   :ensure t
+  :config (load-theme 'base16-default-dark)
+)
+
+(use-package recentf
+  :ensure t
+  :config (setq recentf-max-menu-items 25)
+  :init (recentf-mode 1)
+)
+
+(use-package eshell-syntax-highlighting
+  :after esh-mode
+  :demand t ;; Install if not already installed.
   :config
-  (load-theme 'base16-default-dark)
+  ;; Enable in all Eshell buffers.
+  (eshell-syntax-highlighting-global-mode +1)
+)
+
+(use-package helm
+  :init
+    (require 'helm-config)
+    (setq helm-split-window-in-side-p t
+          helm-move-to-line-cycle-in-source t
+	  helm-mode-line-string nil
+	  helm-display-header-line nil)
+  :config 
+    (helm-mode 1) ;; Most of Emacs prompts become helm-enabled
+    (helm-autoresize-mode 1) ;; Helm resizes according to the number of candidates
+    (global-set-key (kbd "C-x b") 'helm-buffers-list) ;; List buffers ( Emacs way )
+    (define-key evil-ex-map "b" 'helm-buffers-list) ;; List buffers ( Vim way )
+    (global-set-key (kbd "C-x r b") 'helm-bookmarks) ;; Bookmarks menu
+    (global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Finding files with Helm
+    (global-set-key (kbd "M-x") 'helm-M-x)  ;; Improved M-x menu
+    (global-set-key (kbd "C-x C-r") 'helm-recentf)
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  :ensure t
 )
 
 ;; Key Bindings
 (global-set-key (kbd "C-x C-x") 'evil-mode)
 (global-set-key (kbd "M-a")     'neotree-toggle)
 (global-set-key (kbd "C-x C-l") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
 
 (global-set-key (kbd "M-l") 'windmove-right) 
 (global-set-key (kbd "M-h") 'windmove-left) 
@@ -106,18 +133,5 @@
 (global-set-key (kbd "M-K") 'enlarge-window)
 (global-set-key (kbd "M-J") 'shrink-window)
 
-;; jk to enter normal mode in evil
-(setq key-chord-two-keys-delay 0.5)
-(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-(key-chord-mode 1)
-
-;; Unable evil-mode mappings while in neotree
-(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
-(evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
-(evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
-(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
-(evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
-(evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
-(evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
-(evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
-(evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
